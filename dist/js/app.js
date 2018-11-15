15c5750dd6e1144fb75768c9859192747142ccd6
Martin Gamnitzer <kontakt@martin-gamnitzer.de>
2018-11-15 13:17:53 +0100

    removed duplicate jQuery init

--- dist/js/app.js
+++ dist/js/app.js
@@ -177,19 +177,12 @@ $(function() {
     }
   });
 
-
-$(document).ready(function() {
-
   // Check for click events on the navbar burger icon
   $(".navbar-burger").click(function() {
-
       // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
       $(".navbar-burger").toggleClass("is-active");
       $(".navbar-menu").toggleClass("is-active");
-
   });
-});
-
 
   tArea[0].addEventListener('paste', function(e) {
     e.preventDefault();
